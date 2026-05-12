64c1191a1f3c1ab9ca7bc4f7dce4f3d64fa94a0a
Aleksandar Dlabač <aleksandar@dlabac.com>
2026-05-12 17:53:20 +0200

    Dodati komentari skripti Observer

--- Assets/_3DStealthGame/Scripts/Observer.cs
+++ Assets/_3DStealthGame/Scripts/Observer.cs
@@ -2,38 +2,38 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
-    public Transform player;
-    bool PlayerInRange = false;
+    public Transform player;    // Referenca ka Transform komponenti igrača
+    bool PlayerInRange = false;    // Flag koji govori da li je igrač ušao u vidno polje neprijatelja
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform == player)
+        if(other.transform == player)    // Da li je objekat koji je ušao u collider igrač?
         {
-            PlayerInRange = true;
+            PlayerInRange = true;    // Postavi odgovarajući flag
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.transform == player)
+        if(other.transform == player)    // Da li je objekat koji je izašao iz collidera igrač?
         {
-            PlayerInRange = false;
+            PlayerInRange = false;    // Postavi odgovarajući flag
         }
     }
 
     void Update()
     {
-        if (PlayerInRange)
+        if (PlayerInRange)  // Da li je igrač u vidnom polju? Ako jeste, treba provjeriti da li ga neki objekat zaklanja
         {
-            Vector3 direction = player.position - transform.position + Vector3.up;
-            Ray ray = new Ray(transform.position, direction);
-            RaycastHit raycastHit;
+            Vector3 direction = player.position - transform.position + Vector3.up;    // Odredi pravac pogleda posmatrača do tačke koja je 1 m iznad tačke u kojoj igrač trenutno stoji
+            Ray ray = new Ray(transform.position, direction);    // Kreiraj polupravu/zrak od tačke pogleda u datom pravcu
+            RaycastHit raycastHit;    // Promjenljiva u kojoj će biti rezultat raycastinga
 
-            if(Physics.Raycast(ray, out raycastHit))
+            if(Physics.Raycast(ray, out raycastHit))    // Da li se poluprava pogleda siječe sa nekim objektom?
             {
-                if (raycastHit.collider.transform == player)
+                if (raycastHit.collider.transform == player)    // Da li je taj objekat igrač?
                 {
-                    Debug.Log("Vidim te!");
+                    Debug.Log("Vidim te!");    // Ispiši u log da je igrač ugledan
                 }
             }
         }
